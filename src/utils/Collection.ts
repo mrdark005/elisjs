c7809549176f43da072652683bc9d22a4f0f894f
aloima <acar.aloima@gmail.com>
2021-07-11 14:10:21 +0300

    feat(utils): added some methods to the Collection utility.

--- src/utils/Collection.ts
+++ src/utils/Collection.ts
@@ -1,10 +1,32 @@
 class Collection<K, V> extends Map<K, V> {
-  array(): V[] {
+  array() {
     return Array.from(this.values());
   }
 
-  keyArray(): K[] {
+  keyArray() {
     return Array.from(this.keys());
+  }
+
+  find(cb: (value: V, index: number, array: V[]) => boolean) {
+    return this.array().find(cb);
+  }
+
+  filter(cb: (value: V, index: number, array: V[]) => boolean) {
+    return this.array().filter(cb);
+  }
+
+  some(cb: (value: V, index: number, array: V[]) => boolean) {
+    return this.array().some(cb);
+  }
+
+  map(cb: (value: V, index: number, array: V[]) => any) {
+    return this.array().map(cb);
+  }
+
+  // TODO: integrate util.inspect() to detect the objects.
+  at(value: V) {
+    const data = this.find((v) => v == value);
+    return data ? this.array().indexOf(data) : -1;
   }
 }
 
